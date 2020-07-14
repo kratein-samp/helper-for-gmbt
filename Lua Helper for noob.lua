306288834b10cd4c4b15e84f20e5388f4d704c42
kratein <51368188+kratein-samp@users.noreply.github.com>
2020-07-14 13:55:59 +0300

    Update Lua Helper for noob.lua

--- Lua Helper for noob.lua
+++ Lua Helper for noob.lua
@@ -13,8 +13,8 @@ u8 = encoding.UTF8
 
 update_state = false
 
-local script_vers = 2
-local script_vers_text = "1.1"
+local script_vers = 1
+local script_vers_text = "1.0"
 
 local script_path = thisScript().path
 local script_url = "https://raw.githubusercontent.com/kratein-samp/helper-for-arp/master/Lua%20Helper%20for%20noob.lua"
@@ -26,9 +26,9 @@ function main()
 	if not isSampLoaded() or not isSampfuncsLoaded then return end
 	while not isSampAvailable() do wait(100) end
 
-	sampAddChatMessage("[ADVANCE HELPER] Âàø ëè÷íûé ïîìîùíèê, óñïåøíî çàïóùåí.",-1)
-	sampAddChatMessage("[ADVANCE HELPER] ×òîáû óçíàòü êîìàíäû, ââåäèòå: /helpc.",-1)
-	sampAddChatMessage("[ADVANCE HELPER] Ñâÿçü ñ àäìèíèñòðàòîðîì: https://vk.com/kratein",-1)
+	sampAddChatMessage("[ADVANCE HELPER] Ваш личный помощник, успешно запущен.",-1)
+	sampAddChatMessage("[ADVANCE HELPER] Чтобы узнать команды, введите: /helpc.",-1)
+	sampAddChatMessage("[ADVANCE HELPER] Связь с администратором: https://vk.com/kratein",-1)
 
 	sampRegisterChatCommand("clearchat",cc)
 
@@ -39,8 +39,8 @@ function main()
 		if status == dlstatus.STATUS_ENDDOWNLOADDATA then 
 			updateini = inicfg.load(nil, update_path)
 			if tonumber(updateini.info.vers) > script_vers then 
-				sampAddChatMessage("Åñòü îáíîâëåíèå! Âåðñèÿ: " .. updateini.info.vers_text,-1)
-				sampAddChatMessage("Âàøà âåðñèÿ: " .. script_vers_text,-1)
+				sampAddChatMessage("Есть обновление! Версия: " .. updateini.info.vers_text,-1)
+				sampAddChatMessage("Ваша версия: " .. script_vers_text,-1)
 				update_state = true
 			end
 			os.remove(update_path)
@@ -52,7 +52,7 @@ function main()
 		if update_state then
 			downloadUrlToFile(script_url,script_path,function(id, status)
 				if status == dlstatus.STATUS_ENDDOWNLOADDATA then 
-					sampAddChatMessage("îáíîâëåí", -1)
+					sampAddChatMessage("обновлен", -1)
 					thisScript():reload()
 				end
 			end)
@@ -66,5 +66,5 @@ function cc()
 	memory.fill(sampGetChatInfoPtr() + 306, 0x0, 25200)
 	memory.write(sampGetChatInfoPtr() + 306, 25562, 4, 0x0)
 	memory.write(sampGetChatInfoPtr() + 0x63DA, 1, 1)
-	sampAddChatMessage("×àò î÷èùåí",-1)
+	sampAddChatMessage("Чат очищен",-1)
 end
